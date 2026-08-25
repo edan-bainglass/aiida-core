bf6e7ee91e8a69ad70b432798271bbc7c34fac9e
Edan Bainglass <edan.bainglass@gmail.com>
2026-08-25 03:43:02 +0200

    Improve `ModelsNamespace` typing

--- redesign/entity.py
+++ redesign/entity.py
@@ -5,15 +5,15 @@ import typing as t
 
 from fields import field
 from models import ModelsNamespace
+from typing_extensions import Self
 
 from aiida.orm.implementation import BackendEntity
 
-EntityType = t.TypeVar('EntityType', bound='Entity')
 BackendEntityType = t.TypeVar('BackendEntityType', bound=BackendEntity)
 
 
 class Entity(abc.ABC, t.Generic[BackendEntityType]):
-    models: ModelsNamespace[Entity[BackendEntityType]] = ModelsNamespace()
+    models: ModelsNamespace[Self] = ModelsNamespace()
 
     def __init__(self, backend_entity: BackendEntityType, **kwargs):
         super().__init__(**kwargs)
