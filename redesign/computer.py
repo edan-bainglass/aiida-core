b8cd37dfe4f92b7ebda02738385dc1585d97ef08
Edan Bainglass <edan.bainglass@gmail.com>
2026-08-26 03:05:56 +0200

    Add `Computer` entity

---
+++ redesign/computer.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+import typing as t
+
+from entity import Entity
+from fields import ModelField, field
+
+from aiida.manage.manager import get_manager
+from aiida.orm.implementation import BackendComputer, StorageBackend
+
+
+class Computer(Entity[BackendComputer]):
+    def __init__(
+        self,
+        hostname: str,
+        transport_type: str,
+        scheduler_type: str,
+        label: str | None = None,
+        description: str = '',
+        metadata: dict[str, t.Any] | None = None,
+        backend: StorageBackend | None = None,
+        **kwargs,
+    ):
+        backend = backend or get_manager().get_profile_storage()
+        backend_entity = backend.computers.create(
+            hostname=hostname,
+            transport_type=transport_type,
+            scheduler_type=scheduler_type,
+            label=label,
+            description=description,
+            metadata=metadata,
+        )
+        super().__init__(backend_entity, **kwargs)
+
+    @field(model_field_info=ModelField(''))
+    def description(self) -> str:
+        """The description of the computer."""
+        return self._backend_entity.description
+
+    @description.setter
+    def description(self, value: str) -> None:
+        self._backend_entity.description = value
+
+    @field
+    def label(self) -> str:
+        """The label of the computer."""
+        return self._backend_entity.label
+
+    @field
+    def hostname(self) -> str:
+        """The hostname of the computer."""
+        return self._backend_entity.hostname
+
+    @field
+    def transport_type(self) -> str:
+        """The transport type of the computer."""
+        return self._backend_entity.get_transport_type()
+
+    @field
+    def scheduler_type(self) -> str:
+        """The scheduler type of the computer."""
+        return self._backend_entity.get_scheduler_type()
+
+    @field(model_field_info=ModelField(default_factory=dict))
+    def metadata(self) -> dict[str, t.Any]:
+        """The metadata of the computer."""
+        return self._backend_entity.get_metadata()
+
+    @classmethod
+    def get_one(cls, pk: int) -> Computer | None:
+        """Get a computer by primary key."""
+        from aiida import orm
+
+        return orm.Computer.collection.get(pk=pk)
