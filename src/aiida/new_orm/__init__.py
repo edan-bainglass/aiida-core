fa2b3888ff536b12e88bc1d2cae1415366031c51
Edan Bainglass <edan.bainglass@gmail.com>
2026-09-08 13:08:37 +0200

    Update module imports

--- src/aiida/new_orm/__init__.py
+++ src/aiida/new_orm/__init__.py
@@ -9,6 +9,7 @@ from .cli import *
 from .columns import *
 from .fields import *
 from .installed import *
+from .modeling import *
 from .models import *
 from .node_models import *
 from .singlefile import *
@@ -25,8 +26,13 @@ __all__ = (
     'CreateModel',
     'EntityCliCreateSpec',
     'EntityModel',
+    'EntityModelProjection',
     'InstalledCode',
     'ModelFieldInfo',
+    'ModelMetadata',
+    'ModelProjection',
+    'ModelSerializerInfo',
+    'ModelValidatorInfo',
     'ModelsNamespace',
     'NodeAttribute',
     'NodeAttributeConfig',
@@ -37,13 +43,18 @@ __all__ = (
     'ReadModel',
     'SinglefileData',
     'Storable',
-    'SupportedModel',
     'UpdateModel',
     'attribute',
     'attributes_column',
     'column',
     'iter_attributes',
     'iter_columns',
+    'iter_model_serializers',
+    'iter_model_validators',
+    'make_model_serializer',
+    'make_model_validator',
+    'model_serializer',
+    'model_validator',
 )
 
 # fmt: on
